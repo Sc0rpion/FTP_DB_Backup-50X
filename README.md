17341dc3a9c27ba6501af19a7ca26de6dcfd8aa5
Sc0rpion <johndoe@example.com>
2018-10-29 21:03:55 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# FTP_DB_Backup-50X
+
+FTP_DB_Backup-50X .sh script to create backups via ftp payload.
+You will need any unix machine (pc, nas, raspberry, ...) and PS4 with 5.05
+
+Run ftp payload on ps4 and start db_backup.sh
+By default, the BackUp will be executed in the folder where the location script ./backups/ (the date and time the script was run)
+
+BackUp structure is fully compatible with stooged DB_SG_Backup-50X structure
+
+---------------------------------------------------------------------------------------------------------------------------------------
+
+FTP_DB_Backup-50X .sh скрипт для создания backup'ов через ftp payload. 
+Вам потребуется любая unix машина (pc, nas, raspberry, ...) и PS4 с 5.05
+
+Запускаете ftp payload на ps4 и стартуете db_backup.sh
+По умолчанию BackUp будет выполнен в папку нахождения скрипта ./backups/(дата и время выполнения скрипта)
+
+Структура BackUp'а полностью совместима со структурой DB_SG_Backup-50X от stooged
---
+++ db_backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+
+echo "Starting FTP BackUp."
+
+pathofbackup=`dirname $0`/BackUps
+ip_ps4="192.168.1.108"
+nowdat=$(date "+%m_%d_%Y_%H.%M.%S")
+
+	download_path() {
+	    local url=${1}
+		printf "Downloading ${url}: "
+	    echo -n "   "
+	    wget -r --no-parent -P ${pathofbackup}/${nowdat} --progress=dot ${url} 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+	    echo -ne "\b\b\b\b"
+	    echo "DONE"
+	}
+
+	download_file() {
+	    local url=${1}
+		printf "Downloading ${url}: "
+	    echo -n "    "
+	    wget -P ${pathofbackup}/${nowdat}/DB_Dackup --progress=dot ${url} 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+	    echo -ne "\b\b\b\b"
+	    echo " DONE"
+	}
+
+	download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/app.db"
+	download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/addcont.db"
+	download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/av_content_bg.db"
+                         
+	download_path "ftp://${ip_ps4}:1337/system_data/savedata"
+	download_path "ftp://${ip_ps4}:1337/user/home"
+	download_path "ftp://${ip_ps4}:1337/user/trophy"
+	download_path "ftp://${ip_ps4}:1337/user/license"
+	download_path "ftp://${ip_ps4}:1337/user/settings"
+	download_path "ftp://${ip_ps4}:1337/user/system/webkit/secure"
+	download_path "ftp://${ip_ps4}:1337/user/system/webkit/webbrowser"
+	download_path "ftp://${ip_ps4}:1337/system_data/priv/home"
+	download_path "ftp://${ip_ps4}:1337/system_data/priv/license"
+	download_path "ftp://${ip_ps4}:1337/system_data/priv/activation"
+	
+	mv ${pathofbackup}/${nowdat}/$ip_ps4:1337 ${pathofbackup}/${nowdat}/UserData 
+	
+	size_this=$(du -sh ${pathofbackup}/${nowdat}| cut -f -1)
+	size_all=$(du -sh ${pathofbackup} | cut -f -1)
+	
+	echo "FTP BackUp Complete.  Developed by Sc0rpion"
+	echo "BackUp folder ${pathofbackup}/${nowdat} size = ${size_this}, size all BackUps = ${size_all}"
