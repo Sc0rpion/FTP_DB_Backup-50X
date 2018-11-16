bba7f634d306d6cf240d7e2470e5021a9f695949
Sc0rpion <7339871+Sc0rpion@users.noreply.github.com>
2018-11-17 01:25:28 +0600

    Version 2.0
    Now its can backup optical disk from PS4 via FTP

--- db_backup.sh
+++ db_backup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
-
-echo "Starting FTP BackUp."
+echo "Starting FTP BackUp..."
 
 pathofbackup=`dirname $0`/BackUps
-ip_ps4="192.168.1.108"
+ip_ps4="192.168.1.102"					#Change for your IP Adress PS4 Console
 nowdat=$(date "+%m_%d_%Y_%H.%M.%S")
 
 	download_path() {
@@ -25,25 +24,42 @@ nowdat=$(date "+%m_%d_%Y_%H.%M.%S")
 	    echo " DONE"
 	}
 
-	download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/app.db"
-	download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/addcont.db"
-	download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/av_content_bg.db"
+	backup_data() {
+		download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/app.db"
+		download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/addcont.db"
+		download_file "ftp://${ip_ps4}:1337/system_data/priv/mms/av_content_bg.db"
                          
-	download_path "ftp://${ip_ps4}:1337/system_data/savedata"
-	download_path "ftp://${ip_ps4}:1337/user/home"
-	download_path "ftp://${ip_ps4}:1337/user/trophy"
-	download_path "ftp://${ip_ps4}:1337/user/license"
-	download_path "ftp://${ip_ps4}:1337/user/settings"
-	download_path "ftp://${ip_ps4}:1337/user/system/webkit/secure"
-	download_path "ftp://${ip_ps4}:1337/user/system/webkit/webbrowser"
-	download_path "ftp://${ip_ps4}:1337/system_data/priv/home"
-	download_path "ftp://${ip_ps4}:1337/system_data/priv/license"
-	download_path "ftp://${ip_ps4}:1337/system_data/priv/activation"
+		download_path "ftp://${ip_ps4}:1337/system_data/savedata"
+		download_path "ftp://${ip_ps4}:1337/user/home"
+		download_path "ftp://${ip_ps4}:1337/user/trophy"
+		download_path "ftp://${ip_ps4}:1337/user/license"
+		download_path "ftp://${ip_ps4}:1337/user/settings"
+		download_path "ftp://${ip_ps4}:1337/user/system/webkit/secure"
+		download_path "ftp://${ip_ps4}:1337/user/system/webkit/webbrowser"
+		download_path "ftp://${ip_ps4}:1337/system_data/priv/home"
+		download_path "ftp://${ip_ps4}:1337/system_data/priv/license"
+		download_path "ftp://${ip_ps4}:1337/system_data/priv/activation"
 	
-	mv ${pathofbackup}/${nowdat}/$ip_ps4:1337 ${pathofbackup}/${nowdat}/UserData 
+		mv ${pathofbackup}/${nowdat}/$ip_ps4:1337 ${pathofbackup}/${nowdat}/UserData 
+	}
+
+	backup_disk() {
+		download_path "ftp://${ip_ps4}:1337/mnt/disc"
+		mv ${pathofbackup}/${nowdat}/$ip_ps4:1337/mnt/* ${pathofbackup}/${nowdat}/ 
+		rm -rf ${pathofbackup}/${nowdat}/$ip_ps4:1337
+	}
+	
+	while true; do
+	    read -p "What do you want to backup,personal data - Y, optical disk - N?" yn
+	    case $yn in
+	        [Yy]* ) backup_data;break;;
+	        [Nn]* ) backup_disk;break;;
+	        * ) echo "Please answer personal data - Y, optical disk - N.";;
+	    esac
+	done
 	
 	size_this=$(du -sh ${pathofbackup}/${nowdat}| cut -f -1)
 	size_all=$(du -sh ${pathofbackup} | cut -f -1)
 	
-	echo "FTP BackUp Complete.  Developed by Sc0rpion"
 	echo "BackUp folder ${pathofbackup}/${nowdat} size = ${size_this}, size all BackUps = ${size_all}"
+	echo "FTP BackUp Complete.  Developed by Sc0rpion"
